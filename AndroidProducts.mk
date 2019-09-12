e5dd7336b271aa1f7356b8d262c303bda67af051
ivanmeler <i_ivan@windowslive.com>
2019-09-12 11:23:25 +0000

    Q time

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,8 @@
 PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/lineage.mk \
 	$(LOCAL_DIR)/lineage_gracelte.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_gracelte-eng \
+    lineage_gracelte-user \
+    lineage_gracelte-userdebug
--- vendorsetup.sh
+++
@@ -1,3 +0,0 @@
-add_lunch_combo lineage_gracelte-eng
-add_lunch_combo lineage_gracelte-userdebug
-add_lunch_combo lineage_gracelte-user
